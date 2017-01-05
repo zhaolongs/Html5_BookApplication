54bcda045b2acaa3d6143de160ba093190ad0932
zhaolong <928343994@qq.com>
2017-01-06 02:04:16 +0800

    `删除书籍分类功能添加

--- book/main/manager/js/book_class_list.js
+++ book/main/manager/js/book_class_list.js
@@ -60,83 +60,147 @@ function addBookClassRequest() {
 
 }
 
-//删除书籍分类
-function deleteBookClassRequest() {
-    var url = deleteBookClassRequest();
-}
+
 //修改书籍分类
 function updateBookClassRequest() {
     var url = updateBookClassRequest();
 }
 
 
-
-function createTable(result){
+function createTable(result) {
 
     var obj = eval('(' + result + ')');
 
     var arr = obj.contentList;
 
     var $table = $('#book_class_table');
+    $("#book_class_table  tr:not(:first)").remove();
 
-    for(var i=0;i<arr.length;i++){
-
-        var child = arr[i];
-
-        var $td1 = $('<th class="bname">'+child.bcname+'</th>');
-        var $td2 = $('<td class="bdesc">'+child.bcdesc+'</td>');
-
-        var $td3 = $('<td class="balert"><input class="balert_button" type="button" value="删除"></td>');
-
+    $table.append($(
+        '<tr>' +
+        '<th class="bname">类名</th>' +
+        '<th class="bname">描述</th>' +
+        '<th class="bname">操作</th>' +
+        '</tr>'
+    ));
+    if (arr.length == 0) {
         var $tr = $('<tr></tr>');
+        var $td1 = $('<th class="bname" colspan="3">' + '无数据，请点击添加' + '</th>');
         $tr.append($td1);
-        $tr.append($td2);
-        $tr.append($td3);
-
         $table.append($tr);
-    }
-    $table.css({
-        "height":"11.1rem",
-        "width":"6.4rem",
-        "margin-left":"0.5rem",
-        "margin-right":"0.5rem",
-        "font-size":"0.14rem",
-        "border:":"solid 1px #47802f",
-        "table-layout":"fixed"
-    });
 
-    $(".bname").css({
-        "height":"0.2rem",
-        "width":"2rem",
-        "font-size":"0.24rem",
-        "border":"solid 0.5px #47802f",
-        "white-space":"nowrap",
-        "text-overflow":"ellipsis",
-        "overflow":"hidden"
-    });
-    $(".bdesc").css({
-        "height":"0.2rem",
-        "width":"2.5rem",
-        "font-size":"0.2rem",
-        "border":"solid 0.5px #47802f",
-        "white-space":"nowrap",
-        "text-overflow":"ellipsis",
-        "overflow":"hidden"
-    });
-    $(".balert").css({
-        "height":"0.2rem",
-        "width":"2rem",
-        "font-size":"0.14rem",
-        "border":"solid 0.5px #47802f",
-        "white-space":"nowrap",
-        "text-overflow":"ellipsis",
-        "overflow":"hidden"
-    });
+        $table.css({
+            "height": "auto",
+            "width": "6.4rem",
+            "margin-left": "0.5rem",
+            "margin-right": "0.5rem",
+            "font-size": "0.14rem",
+            "border:": "solid 1px #47802f",
+            "table-layout": "fixed"
+        });
+        $(".bname").css({
+            "height": "0.2rem",
+            "width": "2rem",
+            "font-size": "0.24rem",
+            "border": "solid 0.5px #47802f",
+            "white-space": "nowrap",
+            "text-overflow": "ellipsis",
+            "overflow": "hidden"
+        });
 
-    $('.balert_button').css(
-        {
-            "width":"1.8rem"
+    } else {
+        for (var i = 0; i < arr.length; i++) {
+
+            var child = arr[i];
+
+            var $td1 = $('<th class="bname">' + child.bcname + '</th>');
+            var $td2 = $('<td class="bdesc" >' + child.bcdesc + '</td>');
+
+            var $td3 =
+                $('<td class="balert">' +
+                    '<input bcuuid=' + child.bcuuid +
+                    '  class="balert_button"  type="button" ' +
+                    '  value="删除"  onclick="bookClassListTableItemClick(this)" >' +
+                    '</td>');
+
+            var $tr = $('<tr></tr>');
+            $tr.append($td1);
+            $tr.append($td2);
+            $tr.append($td3);
+
+            $table.append($tr);
         }
-    );
 
+        $table.css({
+            "height": "auto",
+            "width": "6.4rem",
+            "margin-left": "0.5rem",
+            "margin-right": "0.5rem",
+            "font-size": "0.14rem",
+            "border:": "solid 1px #47802f",
+            "table-layout": "fixed"
+        });
+
+        $(".bname").css({
+            "height": "0.2rem",
+            "width": "2rem",
+            "font-size": "0.24rem",
+            "border": "solid 0.5px #47802f",
+            "white-space": "nowrap",
+            "text-overflow": "ellipsis",
+            "overflow": "hidden"
+        });
+        $(".bdesc").css({
+            "height": "0.2rem",
+            "width": "2.5rem",
+            "font-size": "0.2rem",
+            "border": "solid 0.5px #47802f",
+            "white-space": "nowrap",
+            "text-overflow": "ellipsis",
+            "overflow": "hidden"
+        });
+        $(".balert").css({
+            "height": "0.2rem",
+            "width": "2rem",
+            "font-size": "0.14rem",
+            "border": "solid 0.5px #47802f",
+            "white-space": "nowrap",
+            "text-overflow": "ellipsis",
+            "overflow": "hidden"
+        });
+
+        $('.balert_button').css(
+            {
+                "width": "1.8rem"
+            }
+        );
+    }
+
+
+}
+
+
+function bookClassListTableItemClick(detalTag) {
+    var bcuuid = detalTag.getAttribute("bcuuid");
+    deleteBookClassRequest(bcuuid);
+}
+
+//删除书籍分类
+function deleteBookClassRequest(bookClassUUid) {
+    var url = deleteBookClassURL();
+    $.post(
+        url,
+        {
+            "bookUuid": encodeURI(bookClassUUid),
+        },
+        function (data, textStatus) {
+            if (textStatus == "success") {
+                console.log("响应数据为 " + decodeURI(data) + " 请求结果为  " + textStatus);
+                alert("删除成功");
+                getBookClassListData();
+            }else{
+                alert("删除成功 异常");
+                console.log("异常 "+data);
+            }
+        }, "json");
 }
