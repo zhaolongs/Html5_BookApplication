2f319b2d6fe934309e5402d177df285fe3272e18
zhaolong <928343994@qq.com>
2017-01-06 02:06:41 +0800

    `细节修复

--- book/main/manager/js/httpHelper.js
+++ book/main/manager/js/httpHelper.js
@@ -2,39 +2,39 @@
  * Created by androidlongs on 17/1/3.
  */
 
-function baseURL(){
-     return "http://localhost:10008/Book/";
+function baseURL() {
+    return "http://localhost:10008/Book/";
 }
 
-function getBookListURL(){
+function getBookListURL() {
 
-     return baseURL() +"/book/list?time="+new Date();
+    return baseURL() + "/book/list?time=" + new Date();
 }
-function getBookClassListURL(){
-     return baseURL() +"manager/getbookclass?time="+new Date();
-}
-
-function addBookURL(){
-
-}
-function addBookClassURL(){
-
-     return baseURL() +"manager/addbookclass?time="+new Date();
+function getBookClassListURL() {
+    return baseURL() + "manager/getbookclass?time=" + new Date();
 }
 
-function deleteBookURL(){
+function addBookURL() {
+    return baseURL() + "/manager/addbook?time=" + new Date();
+}
+function addBookClassURL() {
+
+    return baseURL() + "manager/addbookclass?time=" + new Date();
+}
+
+function deleteBookURL() {
 
 }
-function deleteBookClassURL(){
-
+function deleteBookClassURL() {
+    return baseURL() + "manager/deletebookclass?time=" + new Date();
 }
 
 //修改书籍分类信息
-function updateBookClassURL(){
+function updateBookClassURL() {
 
 }
 
 //修改书籍信息
-function updateBookURL(){
+function updateBookURL() {
 
 }
